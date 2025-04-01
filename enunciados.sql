26de51458288c741662ff462bc67f2f0b1203666
Daniel Saldarriaga López <dsaldarriaga@prevalentware.com>
2025-04-01 06:00:44 -0500

    Create enunciados.sql

---
+++ enunciados.sql
@@ -0,0 +1,31 @@
+0. Realizar un análisis exploratorio de datos sobre cultivos, precios, fincas, lotes, clientes, usuarios, recogidas y despachos.
+
+1. Encontrar los cultivos cuyo precio promedio fue superior a un dólar durante el año 2023.
+
+2. Listar las fincas que han tenido más de 10 lotes cultivados.
+
+3. Identificar los usuarios que han recogido más de 1000 unidades en total.
+
+4. Determinar los meses en los que la cantidad despachada superó las 8.000.000 de unidades.
+
+5. Calcular los lotes cuyo ingreso proyectado fue mayor a 100 millones de pesos en 2023. El ingreso proyectado se calcula como precio por cantidad recogida.
+
+6. Listar las fincas que redujeron su producción en más de un 20% entre los años 2022 y 2023.
+
+7. Obtener el promedio de recolecciones por lote y listar aquellos lotes cuyo promedio supera el promedio general del cultivo al que pertenecen.
+
+8. Calcular el incremento o decremento mensual en facturación entre los años 2022 y 2023.
+
+9. Calcular el incremento o decremento mensual en la cantidad de despachos entre los años 2022 y 2023.
+
+10. Calcular el aumento porcentual anual en la cantidad total recogida por cultivo entre los años 2022 y 2023.
+
+11. Calcular las tres mayores recogidas registradas para cada lote.
+
+12. Calcular el porcentaje de contribución de cada recogida al total anual de su respectivo cultivo.
+
+13. Calcular el porcentaje de contribución de cada finca a la producción total de cada año.
+
+14. Obtener el puesto o ranking de cada usuario según la cantidad total de recogida realizada por año, y mostrar los tres primeros.
+
+15. Calcular la cantidad total recogida por cada lote y su posición relativa dentro de su finca, mostrando los tres primeros por finca.
